5f8bdb484778e63c4e493bf340a8b3c29eb080d7
Hafez Divandari <hafezdivandari@gmail.com>
2024-11-15 02:59:38 +0330

    revert getting interval from device code entity

--- examples/src/Repositories/DeviceCodeRepository.php
+++ examples/src/Repositories/DeviceCodeRepository.php
@@ -50,8 +50,7 @@ class DeviceCodeRepository implements DeviceCodeRepositoryInterface
         $deviceCodeEntity->setIdentifier($deviceCode);
         $deviceCodeEntity->setExpiryDateTime(new DateTimeImmutable('now +1 hour'));
         $deviceCodeEntity->setClient($clientEntity);
-        $deviceCodeEntity->setLastPolledAt(new DateTimeImmutable('now -5 second'));
-        $deviceCodeEntity->setInterval(5);
+        $deviceCodeEntity->setLastPolledAt(new DateTimeImmutable());
 
         // The user identifier should be set when the user authenticates on the
         // OAuth server, along with whether they approved the request
